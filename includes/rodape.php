bafe78d3ce8ee1877c2c4843ffc2595459ef83ce
Ivonilson <ivonilsoncardoso@hotmail.com>
2022-03-19 11:53:54 -0300

    update

--- includes/rodape.php
+++ includes/rodape.php
@@ -1,4 +1,4 @@
-﻿<!-- rodapé --->
+﻿<!-- rodapé 
 <br><br>
 <footer  class="footer fixed-bottom bg-light">
 	
@@ -6,4 +6,13 @@
 			<small>&copy;AbgSoluções -  <?=date("Y")?> - abgsolucoes.tec.br </small>
 	</div>
 
+</footer>--->
+
+<!-- rodapé --->
+<footer  class="sticky-footer">
+	<div class="container">
+		<div class="text-center">
+			<small>&copy;AbgSoluções -  <?=date("Y")?> - abgsolucoes.tec.br </small>
+		</div>
+	</div>
 </footer>
